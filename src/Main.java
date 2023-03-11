7945e021524341a874760797169c789e42edf0d0
AdrianaB <adrianamorar94@yahoo.com>
2023-03-11 09:08:40 +0000

    First homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        person1.setName("Andra");
+        person2.setName("Bogdan");
+        person3.setName("Maria");
+        person1.setAge(24);
+        person2.setAge(32);
+        person3.setAge(19);
+        person1.setMarried(true);
+        person2.setMarried(false);
+        person3.setMarried(true);
+
+
+
+        System.out.println("Name: " + person1.getName() + " have " + person1.getAge() + " years " + person1.getMarried() + " married");
+        System.out.println("Name: " + person2.getName() + " have " + person2.getAge() + " years " + person2.getMarried() + " married");
+        System.out.println("Name: " + person3.getName() + " have " + person3.getAge() + " years " + person3.getMarried() + " married");
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,40 @@
+public class Person {
+    private String name;
+    private int age;
+
+    private boolean married;
+
+
+    public Person() {
+
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String newName) {
+
+        this.name = newName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+
+        this.age = newAge;
+    }
+
+    public boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married1) {
+        married = true;
+    }
+
+}
+
